ce780a209c530823840140deef84167d31c94074
Robert <roberttemta@gmail.com>
2024-10-11 13:52:56 -0500

    New file

--- install.sh
+++ install.sh
@@ -11,3 +11,11 @@ sudo pip3.8 install ansible
 sudo pip3.8 install boto3 botocore awscli
 #change terminal color
 echo "PS1='\e[1;32m\u@\h \w$ \e[m'" >> /home/ec2-user/.bash_profile
+
+# Install git on Master Nodes
+
+sudo yum install git wget curl -y
+git clone https://github.com/roberttemta/Ansible_playbooks_w17.git
+mv Ansible_playbooks_w17/* /home/ec2-user/
+rm -rf Ansible_playbooks_w17
+sudo chown -R ec2-user:ec2-user /home/ec2-user/
